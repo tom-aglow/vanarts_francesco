2e1268da4c66bab42ef626558b4f7fb5babf9102
tom <tom@ohhhh.me>
2017-05-10 13:57:22 -0700

    -- finished wine-list page -- added feature to print menu to the page from .csv file

--- core/Controller.php
+++ core/Controller.php
@@ -7,9 +7,10 @@ class Controller {
 
 
     public function render ($page) {
+        $menuList = ($page == 'menu' || $page == 'wine-list') ? $this->getMenuList($page) : '';
 
         $pageContent = System::buildTemplate($page . '.php', [
-
+            'menuList' => $menuList
         ]);
 
         $html = System::buildTemplate('main.php', [
@@ -19,5 +20,42 @@ class Controller {
         ]);
 
         echo $html;
+    }
+
+    public function getMenuList ($page) {
+        $row = 1;
+        $menu = [];
+        if (($handle = fopen("assets/menu.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $num = count($data);
+                if ($row == 1 || $data[0] == $page) {
+                    for ($c=0; $c < $num; $c++) {
+                        if ($row < 2) {
+                            $menu[$row][] = $data[$c];
+                        } else {
+                            $menu[$row][$menu[1][$c]] = $data[$c];
+                        }
+                    }
+                }
+                $row++;
+            }
+            fclose($handle);
+        }
+        array_splice($menu, 0, 1);
+
+        return $this->reformatMenuList($menu);
+    }
+
+    private function reformatMenuList ($menu) {
+        $newMenu = [];
+
+        foreach ($menu as $item) {
+            $newMenu[$item['subtype']][] = [
+                'name' => $item['name'],
+                'desc' => $item['desc'],
+                'price' => $item['price']
+            ];
+        }
+        return $newMenu;
     }
 }
--- templates/wine-list.php
+++ templates/wine-list.php
@@ -1,14 +1,53 @@
 <article class="col-1 block">
     <h1>Wine list</h1>
 
-    <h2>VINO SPUMANTE</h2>
+    <section>
+        <h2>VINO SPUMANTE</h2>
 
-    <div class="menu-item">
-        <div class="menu-dish">
-            <div class="menu-dish-name">PROSECCO SUPERIORE BRUT</div>
-            <div class="menu-dish-desc">Sommariva, Veneto (glera)</div>
-        </div>
-        <div class="menu-price">65</div>
-    </div>
+        <?foreach ($menuList['vino spumante'] as $item) :?>
+            <div class="menu-item">
+                <div class="menu-dish">
+                    <div class="menu-dish-name"><?=$item['name']?></div>
+                    <div class="menu-dish-desc"><?=$item['desc']?></div>
+                </div>
+                <div class="menu-price"><?=$item['price']?></div>
+            </div>
+        <?endforeach;?>
+
+    </section>
+
+    <section>
+        <h2 class="menu-item__offset">Vino bianco</h2>
+
+        <?foreach ($menuList['vino bianco'] as $item) :?>
+            <div class="menu-item">
+                <div class="menu-dish">
+                    <div class="menu-dish-name"><?=$item['name']?></div>
+                    <div class="menu-dish-desc"><?=$item['desc']?></div>
+                </div>
+                <div class="menu-price"><?=$item['price']?></div>
+            </div>
+        <?endforeach;?>
+
+    </section>
+
+</article>
+
+<article class="col-1 block">
+    <section>
+        <h2>Vino rosso</h2>
+
+        <?foreach ($menuList['vino rosso'] as $item) :?>
+            <div class="menu-item">
+                <div class="menu-dish">
+                    <div class="menu-dish-name"><?=$item['name']?></div>
+                    <div class="menu-dish-desc"><?=$item['desc']?></div>
+                </div>
+                <div class="menu-price"><?=$item['price']?></div>
+            </div>
+        <?endforeach;?>
+
+        <a class="btn btn-primary btn-md" href="#">See full wine list</a>
+    </section>
 
 </article>
