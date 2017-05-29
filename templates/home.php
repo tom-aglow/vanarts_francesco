dc9832eb25281e55b80c3a67fdc8ab96836d5ca8
tom <tom@ohhhh.me>
2017-05-29 11:25:56 -0700

    changed min font-size to 14px -- fixed form elements width in small screens

--- templates/home.php
+++ templates/home.php
@@ -6,7 +6,6 @@
         <h3>Macro Lombardi</h3>
         
         <p>Pellentesque vehicula volutpat lectus, eget cursus nibh congue eu. Ut pellentesque urna vitae enim pharetra, non euismod elit facilisis. Aenean posuere neque id ante consequat, ac ullamcorper tellus dapibus. Etiam hendrerit justo vitae egestas consectetur.</p>
-        <p>Donec dolor orci, ornare nec ornare faucibus, imperdiet id nunc. Ut vehicula, est nec fermentum suscipit, nunc sem elementum justo, eget ullamcorper tellus massa et ligula. Integer vitae nisl ut tortor dictum egestas.</p>
 
         <a class="btn btn-primary btn-md" href="/about">Know more about us</a>
     </section>
@@ -20,7 +19,6 @@
         <h3>TAGLIERINI AL NERO</h3>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum tincidunt orci, venenatis venenatis metus porttitor sed. Nulla facilisi. Morbi pharetra vitae ligula et porttitor.</p>
-        <p>Pellentesque vehicula volutpat lectus, eget cursus nibh congue eu. Ut pellentesque urna vitae enim pharetra, non euismod elit facilisis.</p>
 
         <a class="btn btn-primary btn-md" href="/menu">See our menu</a>
     </section>
@@ -34,7 +32,6 @@
         <h3>Start planning your event</h3>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum tincidunt orci, venenatis venenatis metus porttitor sed. Aenean posuere neque id ante consequat, ac ullamcorper tellus dapibus.Morbi pharetra vitae ligula et porttitor.</p>
-        <p>Suspendisse vitae nibh lacus. Praesent eu elit quis nibh ornare maximus ullamcorper vitae mi. Nunc placerat arcu sed nunc eleifend, at finibus nunc interdum.</p>
 
         <a class="btn btn-primary btn-md" href="/reservation">Book an event</a>
     </section>
